f6b8b657cdbf40c7da3bf753a1ddd8e819ef705e
huangyiwei <huangyiwei@9fbank.cc>
2016-10-13 14:11:38 +0800

    完成到Excel文件的导出

--- db-tools/export_table_columns_to_excel.py
+++ db-tools/export_table_columns_to_excel.py
@@ -3,11 +3,41 @@
 # 适用于Mysql 5.6.x
 
 import sys
+from optparse import OptionParser
 
 import MySQLdb
 import xlwt
 
+# opts, args = getopt.getopt(sys.argv[1:], "hi:o:",
+#                            ("help", "host=", "port=", "user=", "passwd=", "database=", "charset=", "output="))
 
+# 默认参数,数据库配置
+# 如果不在启动的时候传入参数,则需要在此处进行数据库配置
+host = "192.168.45.155"
+port = 3306
+user = "welicai"
+passwd = "welicai"
+db = "db-house-td"
+charset = "utf8"
+
+# 默认参数,导出的文件路径配置
+# 如果不在启动的时候传入参数,则默认在当前目录生成导出的excel文件
+excel_full_path = "./export_table_columns_info.xls"
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+parser = OptionParser(usage="%prog [-f] [-q]", version="%prog 1.0")
+parser.add_option("", "--host", dest="host", default=host, help="config mysql host")
+parser.add_option("", "--port", type="int", dest="port", default=port, help="config mysql port")
+parser.add_option("-u", "--user", dest="user", default=user, help="config mysql login user")
+parser.add_option("-p", "--passwd", dest="passwd", default=passwd, help="config mysql login password")
+parser.add_option("-d", "--database", dest="db", default=db, help="select a mysql database")
+parser.add_option("-c", "--charset", dest="charset", default=charset, help="select a mysql encode")
+parser.add_option("-o", "--output", dest="excel_full_path", default=excel_full_path,
+                  help="config export excel abstract path")
+
+options, args = parser.parse_args(sys.argv)
 
 # 默认参数,定义查询出来的表结构信息中各字段的索引
 table_info_name_index = 0
@@ -18,21 +48,14 @@ column_info_type_index = 1
 column_info_allow_null_index = 3
 column_info_default_index = 5
 column_info_comment_index = 8
-#
-excel_file_path = "./"
-excel_file_name = "export_table_columns_info.xls"
-excel_full_path = excel_file_path + excel_file_name
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 conn = MySQLdb.connect(
-        host='192.168.45.155',
-        port=3306,
-        user='welicai',
-        passwd='welicai',
-        db='db-house-td',
-        charset='utf8'
+        host=options.host,
+        port=options.port,
+        user=options.user,
+        passwd=options.passwd,
+        db=options.db,
+        charset=options.charset
         )
 
 cur = conn.cursor()
@@ -73,8 +96,8 @@ for table_index, table_info in enumerate(tables_list):
 
         print column_index, column_name, column_type, column_allow_null, column_default, column_comment
 
-wbk.save(excel_full_path)
-print "导出表结构到Excel文件成功,文件路径:", excel_full_path
+wbk.save(options.excel_full_path)
+print "导出表结构到Excel文件成功,文件路径:", options.excel_full_path
 
 cur.close()
 conn.commit()
